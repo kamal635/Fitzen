60531180e176c772b9c97580fc86c436eb5e545f
Kamal Ibrahim <kokoangry51@gmail.com>
2025-06-14 12:00:54 +0300

    add image onboarding + create abstract class assets and colors

---
+++ fitzen/lib/core/constant/app_assets.dart
@@ -0,0 +1,8 @@
+abstract class AppAssets {
+  //======================== OnBoarding ==================================
+  static const String buildStrength =
+      "assets/images/onboarding/build_strength.png";
+  static const String healthyFood = "assets/images/onboarding/healthy_food.png";
+  static const String startFitness =
+      "assets/images/onboarding/start_fitness.png";
+}
---
+++ fitzen/lib/core/constant/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color primary = Color(0xFF121922);
+  static const Color secondary = Color(0xFFD2BDA5);
+}
