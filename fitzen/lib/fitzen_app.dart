dcc8e995f7197cc5200d03dd457af8baa319dc29
Kamal Ibrahim <kokoangry51@gmail.com>
2025-06-18 15:48:53 +0300

    fix responsive size screen to working on all devices harmoniously

--- fitzen/lib/fitzen_app.dart
+++ fitzen/lib/fitzen_app.dart
@@ -1,8 +1,8 @@
 import 'package:fitzen/core/constant/app_colors.dart';
 import 'package:fitzen/core/routing/app_router.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class FitzenApp extends ConsumerWidget {
   const FitzenApp({super.key});
@@ -11,16 +11,39 @@ class FitzenApp extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final appRouter = ref.watch(appRouterProvider);
 
-    return ScreenUtilInit(
-      designSize: const Size(375, 812),
-      builder: (context, _) {
-        return MaterialApp.router(
-          routerConfig: appRouter,
-          theme: ThemeData(
-            scaffoldBackgroundColor: AppColors.primary,
-            brightness: Brightness.dark,
-          ),
-          debugShowCheckedModeBanner: false,
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        // Determine the current size
+        final screenWidth = constraints.maxWidth;
+
+        // choose designSize based on device size
+        late Size designSize;
+
+        if (screenWidth >= 1000) {
+          //  LapTop or large Tablet
+          designSize = const Size(1280, 800);
+        } else if (screenWidth >= 600) {
+          //  Medium Tablet
+          designSize = const Size(800, 1280);
+        } else {
+          // Mobile
+          designSize = const Size(412, 915);
+        }
+
+        return ScreenUtilInit(
+          designSize: designSize,
+          minTextAdapt: true,
+          splitScreenMode: true,
+          builder: (context, child) {
+            return MaterialApp.router(
+              routerConfig: appRouter,
+              theme: ThemeData(
+                scaffoldBackgroundColor: AppColors.primary,
+                brightness: Brightness.dark,
+              ),
+              debugShowCheckedModeBanner: false,
+            );
+          },
         );
       },
     );
